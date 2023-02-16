dbc3fb67d7051d4c2594e95a333932e3c99f37ad
Sayantan Mondal <62999423+MondalSayantan@users.noreply.github.com>
2023-02-16 18:06:41 +0530

    Time: 1 ms (25.80%), Space: 40.6 MB (53.63%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,19 +15,32 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> answer = new ArrayList<>();
-        inorder(root, answer);
+        
+        if(root == null) {
+            return answer;
+        }
+        
+        addLeftSubTree(root, stack);
+        
+        while(!stack.isEmpty()) {
+            TreeNode currentNode = stack.pop();
+            answer.add(currentNode.val);
+            
+            if(currentNode.right != null) {
+                addLeftSubTree(currentNode.right, stack);
+            }
+        }
+        
         return answer;
     }
     
-    private static void inorder(TreeNode root, List<Integer> answer) {
-        if(root == null)
-            return;
-        
-        inorder(root.left, answer);
-        answer.add(root.val);
-        inorder(root.right, answer);
-        
-        return;
+    private static void addLeftSubTree(TreeNode currentNode, Stack<TreeNode> stack) {
+        stack.push(currentNode);
+        while(currentNode.left != null) {
+            stack.push(currentNode.left);
+            currentNode = currentNode.left;
+        }
     }
 }
